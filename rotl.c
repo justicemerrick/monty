188db15392be4aa8d61fd5c30bfafb46df12e51a
Justice Merrick <106972535+justicemerrick@users.noreply.github.com>
2022-10-07 11:13:47 +0300

    rotates the stack

--- rotl.c
+++ rotl.c
@@ -3,7 +3,7 @@
   *f_rotl- rotates the stack to the top
   *@head: stack head
   *@counter: line_number
-  *Return: no return
+  *Return: no return.
  */
 void f_rotl(stack_t **head,  __attribute__((unused)) unsigned int counter)
 {
